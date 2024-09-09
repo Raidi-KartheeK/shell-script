19114210364b966481dc21e26b2ddd2163aba15e
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-09 18:14:40 +0530

    topic on shell

--- 02-varables.sh
+++ 02-varables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=vardhan
-person2=vishal
+person1=$1
+person2=$2
 
 echo "$person1:: hi $person2 , how are  you?"
 echo "$person2:: hello $person1, i am good .how are you doing?"
