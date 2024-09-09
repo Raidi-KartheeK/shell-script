51b78e31bcb3409e8d97411379bd18c9125e2492
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-09 22:04:45 +0530

    i have change the txt

--- 04-varibles.sh
+++ 04-varibles.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "please enter your username:: "
-read- $USERNAME 
-echo "username entered is: $username"
-echo "please enter your password::"
-read -$PASSWORD
-echo "password entered is: $password"
+echo "please enter your username::"
+
+read USERNAME
+
+echo "username entered is:$USERNAME"
 
