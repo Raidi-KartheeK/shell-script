a059bc20fd3f89efe1a8d92335de3c7c7baf4fed
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-09 17:53:49 +0530

    topic on shell

--- 02-varables.sh
+++ 02-varables.sh
@@ -6,5 +6,8 @@ echo "$person1:: hi $person2 , how are  you?"
 echo "$person2:: hello $person1, i am good .how are you doing?"
 echo "$person1:: i am doing good $person2.whats going on "
 echo "$person2:: i have started learning shell script $person1"
+echo "$person1:: even i am also learning shell $person2"
+echo "$person2:: what topics are covered so far $person1"
+echo "$person1:: aaaa currently basic topics are covered $person2"
 
 
