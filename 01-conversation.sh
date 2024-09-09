f0e59db6f925a845c03e7657e8c240e36cf8198c
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-09 14:05:05 +0530

    i have created new conversation

---
+++ 01-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "raidi:: hi bittu , how are  you?"
+echo "bittu:: hello raidi, i am good .how are you doing?"
+echo "raidi:: i am doing good bittu.whats going on "
+echo "bittu:: i have started learning shell script raidi"
