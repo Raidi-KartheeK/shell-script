f46bedbdb542b4ab7c63062ddc686253a7fff7d5
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-10 10:01:42 +0530

    added new fruit

--- 05-array.sh
+++ 05-array.sh
@@ -1,12 +1,13 @@
 #!/ bin/bash
  
- FRUITS=("APPLE" "KIWI" "ORANGE" "GUWA" "MANGO")
+ FRUITS=("APPLE" "KIWI" "ORANGE" "GUWA" "MANGO" "grape")
 
  echo "First fruit is: ${FRUITS[0]}"
  echo "Second fruit is: ${FRUITS[1]}"
  echo "Third  fruit is: ${FRUITS[2]}"
  echo "Fourth fruit is: ${FRUITS[3]}"
  echo "Fifth fruit is : ${FRUITS[4]}"
+ echo "sixth fruit is : ${FRUITS[5]}
 
  echo "First fruit is: ${FRUITS[@]}"
 
