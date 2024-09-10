3a2a30990c9948fb86ef7a9166531fe8bc9e4552
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-10 11:16:17 +0530

    added new fruit

--- 07-exitstatus.sh
+++ 07-exitstatus.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 lss -ltr
-echo "exit status:$?"
+if[$? -ne 0]; then 
+echo "exit command is failure"
+exit 1]
+Fi
 
 ls -ltr
-echo "exit status $?"
+if[$? -ne 0]; then 
+echo "exit command is failure"
+exit 1]
+Fi
+echo "program is sucess"
