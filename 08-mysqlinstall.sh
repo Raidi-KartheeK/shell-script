58fd22be23f0aae7f15f28c97534bd148392e658
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-10 13:07:47 +0530

    added new fruit

--- 08-mysqlinstall.sh
+++ 08-mysqlinstall.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-USERID=$(ld -u)
+# Check if the user is root
+USERID=$(id -u)
 
-if[$USERID -ne 0]; then
-  echo "you need to be root user to execute this script"
+if [ $USERID -ne 0 ]; then
+  echo "You need to be root user to execute this script"
   exit 1
+fi
 
+# Install MySQL using DNF
+dnf install mysql -y
+
+# Check if the installation was successful
+if [ $? -ne 0 ]; then
+  echo "MySQL installation failed"
+  exit 1
+else
+  echo "MySQL installation is successful"
 fi
