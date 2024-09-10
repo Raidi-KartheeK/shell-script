9b4d34ccda7fc42eddd843390bfa586c94ef9fe9
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-10 10:46:03 +0530

    added new fruit

--- 04-varibles.sh
+++ 04-varibles.sh
@@ -4,12 +4,10 @@ echo "please enter your username::"
 
 read -s USERNAME
 
-echo "username entered is:$USERNAME"
-
 echo "enter your password"
 
 read -s PASSWORD
 
-echo "password entered is:$password"
+
 
 
