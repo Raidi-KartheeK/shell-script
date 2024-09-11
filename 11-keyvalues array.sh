77917f6ccb58774fb59f433f85a8de6283c7009a
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-11 12:39:47 +0530

    added new name details

---
+++ 11-keyvalues array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#how to store the key values pairs
+declare -A details
+details=( [name]=raidi [age]=30 [city]=kgm)
+
+echo "Name is ${details[name]}"
+echo "Name is ${details[age]}"
+
+
