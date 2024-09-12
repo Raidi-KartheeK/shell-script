76b5a5af9a8b9415fec061c0bcbeb2f6c13a2d74
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-12 18:03:48 +0530

    install git

--- 09-functions.sh
+++ 09-functions.sh
@@ -3,9 +3,9 @@
 # Check if the user is root
 R="\e[31m
 G="\e[32m
-y="\e[33m
-b="\e[34m
-
+Y="\e[33m
+B="\e[34m
+USERID=$(id -u)
 CHECK_ROOT(){ #to check the user is having root access are not
                              #     echo "user ID is:$USERID"
         if [ $USERID -ne 0 ]
@@ -18,15 +18,16 @@ then
     VALIDATE(){
     if [ $1 -ne 0 ]
 then 
-        echo  -e"$2 is...$R FAILED $N"
+
+        echo  -e "$2 is...$R FAILED $N"
         exit 1 
 else 
-        echo -e"$2 is...$G SUCCESS $N"
+        echo -e "$2 is...$G SUCCESS $N"
     fi
     } 
     CHECK_ROOT
      
-     dnf installed git
+     dnf list installed git
         if [ $? -ne 0 ]
 then
         echo "git is not instlled , going to install it.."
