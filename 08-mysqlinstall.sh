c06e0fdf64195a02aad6fa951c1ce4571c232d19
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-12 12:25:18 +0530

    added new name details

--- 08-mysqlinstall.sh
+++ 08-mysqlinstall.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 # Check if the user is root
-USERID=$(id -u)
+USERID=$[id -u] #to check the user is having root access are not
 
-if [ $USERID -ne 0 ]; then
-  echo "You need to be root user to execute this script"
-  exit 1
-fi
 
-# Install MySQL using DNF
-dnf install mysql -y
 
-# Check if the installation was successful
-if [ $? -ne 0 ]; then
-  echo "MySQL installation failed"
-  exit 1
-else
-  echo "MySQL installation is successful"
-fi
