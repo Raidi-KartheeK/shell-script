e90a4f16effbdc4f44cbc0c0c75c56032b7ba031
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-12 17:52:36 +0530

    install git

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
- #Check if the user is root
-USERID=$(id -u) #to check the user is having root access are not
-      VALIDATE(){
-      if [ $1 -ne 0 ]
-then
-      echo "$2 is failed"
-      exit 1
-else 
-     echo "$2 is sucess"
-fi
+# Check if the user is root
+R="\e[31m
+G="\e[32m
+y="\e[33m
+b="\e[34m
+
+CHECK_ROOT(){ #to check the user is having root access are not
+                             #     echo "user ID is:$USERID"
+        if [ $USERID -ne 0 ]
+then 
+        echo "please run the script with root user"
+        exit 1
+
+    fi
 }
+    VALIDATE(){
+    if [ $1 -ne 0 ]
+then 
+        echo  -e"$2 is...$R FAILED $N"
+        exit 1 
+else 
+        echo -e"$2 is...$G SUCCESS $N"
+    fi
+    } 
+    CHECK_ROOT
+     
+     dnf installed git
+        if [ $? -ne 0 ]
+then
+        echo "git is not instlled , going to install it.."
+        dnf install git -y
+        VALIDATE $? #exit status check 
+else
+        echo "git is already installed"
+fi
 
-# then 
-#          echo "please run the script with root user"
-#         #  exit 1
+ 
+dnf list install mysql #mysql installation
+if [ $? -ne 0 ]
+then
+        echo "mysql is not installed at...going to install"
+        dnf install mysql -y
+        VALIDATE $? "installing mysql" #exit status check 
 
-#  fi
-#  dnf list installed git  #to dnf list that u installed 
-#          if [ $? -ne 0 ] then
-#          echo "git is not instlled , going to install it.."
-#          dnf install git -y
-#          if [ $? -ne 0] #exit status check 
-#  then
-#          echo "git installed is not sucess ...check it"
-
-#  else
-#          echo "git installed is success"
-#          fi
-#  else
-#          echo "git is allredy installed, noting to do"
-#          fi
-
-#  dnf list install mysql #mysql installation
-#  if [ $? -ne 0 ]
-#  then
-#          echo "mysql is not installed at...going to install"
-#          dnf install mysql -y
-#          if [ $? -ne 0 ] #exit status check 
-#  then
-#          echo "mysql installed ..going to insatll" #if not install install mysql
-#          exit1
-
-#  else 
-#          echo "mysql installed sucess"
-#         fi
-# else
-#          echo "mysql is already installed ...."
-#  fi
+else
+        echo "mysql is already installed ...."
+fi
 
 
 
