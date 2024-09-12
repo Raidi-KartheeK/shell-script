af710f4837e5948b6170047208c4457a616d7522
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-12 12:10:08 +0530

    added new name details

--- 13_conditions.sh
+++ 13_conditions.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 NUM=$1
-if [ $NUM -gt 20 ] then
+
+if [ $NUM -gt 20 ] #gt,lt,eq, -ne,-ge,-le
+then
     echo "Given number: $NUM is grater than 20"
 else
     echo "Given number: $NUM is less than 20"
 fi
+
+
