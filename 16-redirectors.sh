0418a0b7b6328273218b038f05e5658ddbc0d835
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-13 17:01:03 +0530

    install git

---
+++ 16-redirectors.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-practice" #log file
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #$0 command run inside shell script # cut -d "." -f1 (dilimater)
+TIMESTAMP=$(DATE +%Y-%M-%S-%H-%M-%S) #time stamp
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m" #reset color 
+        
+        USERID=$(id -u)   # Check if the user is root
+        
+        CHECK_ROOT(){ 
+            #to check the user is having root access are not  #    echo "user ID is:$USERID"
+        
+         if [ $USERID -ne 0 ]
+    then 
+        echo -e"$R please run the script with root user$N" &>>$LOG_FILE
+        exit 1
+
+    fi
+}
+        VALIDATE(){
+        if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        exit 1 
+    else 
+        echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+    fi
+} 
+    # Run the root check function
+        CHECK_ROOT
+
+# Loop through the provided arguments (packages)
+        for package in $@ # $@ is all arguments pass to it
+        do 
+        dnf list installed $package   &>>$LOG_FILE
+        if [ $? -ne 0 ]
+    then
+        echo "$package is not installed going to install it .."
+        dnf install $package -y
+        VALIDATE $? "installing $package" &>>$LOG_FILE
+    else
+        echo -e "$package is already $Yinstalled .. noting to do$N " &>>$LOG_FILE
+        fi
+    done
+
+
