c87b30d36767c4cc7434e02e3e0a2c00a8abef45
raidi kartheek <karthik.raidi13@gmai.com>
2024-09-13 17:30:38 +0530

    install git

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -56,7 +56,6 @@ N="\e[0m" #reset color
         dnf install $package -y
         VALIDATE $? "installing $package" &>>$LOG_FILE
     else
-        echo -e "$package is already $Y installed .. noting to do $N" &>>$LOG_FILE
-
-        fi
+        echo -e "$package is already $Y installed..noting to do $N" &>>$LOG_FILE
+    fi
     done
