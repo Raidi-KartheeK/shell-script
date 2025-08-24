fb79e7a38527ee981631aa484e3d81394d581d07
Your Name <you@example.com>
2025-08-24 18:58:09 +0530

    msg

--- 20-delete-old-logs.sh
+++ 20-delete-old-logs.sh
@@ -18,10 +18,8 @@ FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 echo "Files: $FILES"
 
 # dont use line, it is reserverd word
-while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-    echo "Deleting line: $line"
-    rm -rf $line
+    echo "Deleting file: $file"
+    rm -rf $file
 done <<< $FILES
-
-
