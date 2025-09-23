5a772bc2bd2a9371e4e6450a10f9f8a4bb6b6997
Your Name <you@example.com>
2025-09-23 11:50:58 +0530

    msg

--- shell-robo-shop/roboshop.sh
+++ shell-robo-shop/roboshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-07c8acf3fa6b923fa" # replace with your SG ID
-ZONE_ID="Z0948150OFPSYTNVYZOY" # replace with your ID
-DOMAIN_NAME="daws86s.fun"
+SG_ID= # replace with your SG ID
+ZONE_ID= # replace with your ID
+DOMAIN_NAME="daws86s.fun" # replace with domain name
 for instance in $@ # mongodb redis mysql
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
